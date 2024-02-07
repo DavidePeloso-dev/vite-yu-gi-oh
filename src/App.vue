8b44da42aec5308f3d21857d5de2d4bc55008600
DavidePeloso-dev <davide.peloso.e@gmail.com>
2024-02-07 23:41:47 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+import axios from "axios";
+import AppCard from "./components/AppCard.vue";
+export default {
+  name: 'App',
+  data() {
+    return {
+      cards: [],
+    }
+  },
+  components: {
+    AppCard
+  },
+  methods: {
+
+  },
+  mounted() {
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0')
+      .then((resp) => {
+        const cardsList = resp.data.data;
+        console.log(cardsList);
+        cardsList.forEach(card => {
+          const cardObj = {
+            name: card.name,
+            archetype: card.archetype,
+            img: card.card_images[0].image_url_small,
+          }
+          this.cards.push(cardObj)
+        });
+      })
+  },
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="row d-flex f-wrap jf-center">
+      <div class="col-3 col-lg-4 col-md-6 col-sm-12" v-for="card in cards">
+        <AppCard :img="card.img" :name="card.name" :type="card.archetype"></AppCard>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: "AppCard",
+    data() {
+        return {
+
+        }
+    },
+    components: {
+    },
+    methods: {
+    },
+    props: {
+        img: String,
+        name: String,
+        type: String,
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src="img" alt="">
+        <div class="info">
+            <h4 class="tx-wt">{{ name }}</h4>
+            <h5>{{ type }}</h5>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.info {
+    padding: 0.5rem;
+    text-align: center;
+    background-color: rgb(226, 150, 51);
+}
+</style>
